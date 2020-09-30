386710efa6de5a2cec60e5e429226ca54172ccc8
“robberr” <robertobalbin1@gmail.com>
2020-09-30 00:20:40 -0500

    versión 1.0.0

---
+++ src/main.js
@@ -0,0 +1,11 @@
+﻿// Import a logger for easier debugging.
+import debug from 'debug';
+const log = debug('app:log');
+// The logger should only be disabled if we’re not in production.
+if (ENV !== 'production') {
+    // Enable the logger.
+    debug.enable('*');
+    log('Logging is enabled!');
+} else {
+    debug.disable();
+}
